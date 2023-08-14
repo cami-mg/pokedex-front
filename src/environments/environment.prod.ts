08acb01742c6b7dc05db85976b001ad549b314f5
camila.galban <camila.galban@fluxit.com.ar>
2023-08-14 17:03:50 -0300

    Revert "changed env settings"
    This reverts commit cf7598ff441cc06922c2aa0d260db179cb15bcd5.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  BASE_URL: "https://pokeapi.co/api/v2/",
-  BACK_URL: "https://pokemon-back.000webhostapp.com"
+  BASE_URL: "https://pokeapi.co/api/v2/"
 };
